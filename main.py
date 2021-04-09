04782314b959e09be17a7e270ab84bc320164254
Siddid <63721267+Siddid-soni@users.noreply.github.com>
2021-04-09 13:34:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,23 @@ class MyLayout(GridLayout):
                 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
                 'outtmpl': '~/Videos/%(title)s.%(ext)s',
                 'mergeoutput': True,
-                'ffmpeg_location': 'ffmpeg-win-2.2.2'
+                'ffmpeg_location': 'ffmpeg/bin',
+                'videoformat': 'mp4'
             }
-            self.ids.ch2.disabled = True
-            self.ids.ch1.disabled = False
+      
         elif y == 'audio':
             options = {
                 'format': 'bestaudio/best',
                 'extractaudio': True,  # extracting audio
                 'audioformat': 'mp3',  # file options and format
                 'outtmpl': "~/Music/%(title)s.%(ext)s",  # name the file the ID of the video
-                'ffmpeg_location': 'ffmpeg-win-2.2.2',
+                'ffmpeg_location': 'ffmpeg/bin',
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',  # Used ffmpeg for converting the file to mp3
                     'preferredcodec': 'mp3',
                     'preferredquality': '192',
                 }]
             }
-            self.ids.ch1.disabled = True
-            self.ids.ch2.disabled = False
 
     def download(self):
         link = self.ids.link.text
